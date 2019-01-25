97ffd7ba88d72316dec44fdc5729757420c651e0
Juho Lehtonen <lehtonenjuho@gmail.com>
2019-01-25 07:01:39 +0200

    Commented out testnet configurations.

--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
 
-var HDWalletProvider = require("truffle-hdwallet-provider")
+//var HDWalletProvider = require("truffle-hdwallet-provider")
 
-var mnemonic = "put your own mnemonic here"
-var infura = "https://rinkeby.infura.io/v3/<id>"
+//var mnemonic = "put your own mnemonic here"
+//var infura = "https://rinkeby.infura.io/v3/<id>"
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -13,12 +13,12 @@ module.exports = {
       port: 8545,
       network_id: "*",
       gas: 5500000,
-    },
+    }/*,
     rinkeby: {
         provider: new HDWalletProvider(mnemonic, infura),
         network_id: "4",
         gas: 5500000,
-    }
+    }*/
   },
   solc: {
     optimizer: {
