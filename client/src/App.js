60d446e10b6087505dc17e96ff8d04fc1e1861f5
Juho Lehtonen <lehtonenjuho@gmail.com>
2019-02-08 07:32:14 +0200

    More informative error when contract is not found from network.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Typography from '@material-ui/core/Typography';
+import Paper from "@material-ui/core/Paper";
 import Marketplace from "./contracts/Marketplace.json";
 import getWeb3 from "./utils/getWeb3";
 import Index from './pages/index';
@@ -8,6 +12,20 @@ import {Translator} from './utils/translator';
 import ipfsClient from "ipfs-http-client";
 
 import "./App.css";
+
+
+const styles = theme => ({
+  root: {
+    paddingLeft: "5%",
+    paddingRight: "5%",
+    paddingTop: "5%",
+  },
+  requestPaper: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+  }
+});
 
 class App extends Component {
   state = { userStatus: -1, web3: null, accounts: null, contract: null };
@@ -29,22 +47,24 @@ class App extends Component {
         deployedNetwork && deployedNetwork.address,
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.intialize);
-      this.props.contractLoaded(instance);
-      this.props.accountsLoaded(accounts);
+      if (instance._address !== null) {
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        this.setState({ web3, accounts, contract: instance }, this.intialize);
+        this.props.contractLoaded(instance);
+        this.props.accountsLoaded(accounts);
 
-      // load IPFS client
-      var ipfs = ipfsClient('localhost', '5001', { protocol: 'http' });
-      ipfs.swarm.peers(function (err) {
-        if (err) {
-          ipfsLoaded(false);
-          console.log("IPFS daemon not found. File upload will be disabled: ", err); 
-        } else {
-          ipfsLoaded(ipfs);
-        }
-      });
+        // load IPFS client
+        var ipfs = ipfsClient('localhost', '5001', { protocol: 'http' });
+        ipfs.swarm.peers(function (err) {
+          if (err) {
+            ipfsLoaded(false);
+            console.log("IPFS daemon not found. File upload will be disabled: ", err); 
+          } else {
+            ipfsLoaded(ipfs);
+          }
+        });
+      }
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -56,7 +76,6 @@ class App extends Component {
 
   intialize = async () => {
     const { contract, accounts } = this.state;
-
     const userStatusResponse = await contract.methods.getUserStatus(accounts[0]).call({ from: accounts[0] });
     const userStatusInt = parseInt(userStatusResponse);
     this.setState({ userStatus: userStatusInt });
@@ -64,12 +83,37 @@ class App extends Component {
   };
 
   render() {
+    const { classes } = this.props;
+    if (this.state.contract === null) {
+      return (<div className={classes.root}>
+      <Paper className={classes.requestPaper} elevation={1}>
+        <Typography variant="h5" component="h3">
+          Contract is not found from selected network...
+        </Typography>
+        <Typography component="p">
+          - Remember to use web3.0 compatible browser or metamask plugin.
+          Learn more: https://metamask.io/ 
+        </Typography>
+        <Typography component="p">
+          - Select Rinkeby network if you want to interact with public contract.
+        </Typography>
+        <Typography component="p">
+          - If you're trying to set up project locally, start ganache-cli and connect
+          browser to localhost:8545 (by default)
+        </Typography>
+      </Paper>
+      </div>);
+    }
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (<Index userStatus={this.state.userStatus} />);
   }
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -92,4 +136,4 @@ const mapDispatchToProps = (dispatch) => {
 export default connect(
 mapStateToProps,
 mapDispatchToProps
-)(App)
+)((withStyles(styles)(App)))
